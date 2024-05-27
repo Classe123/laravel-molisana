b629aba10d4b85fceb4a6bce125b9e51f50b335f
klelia <clelia.fradella@gmail.com>
2024-05-27 10:13:48 +0200

    scaffolding and data

---
+++ database/seeders/ProductsTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Product;
+
+class ProductsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $products = config('db.products');
+        foreach ($products as $product) {
+            $new_product = new Product();
+            $new_product->title = $product['titolo'];
+            $new_product->description = $product['descrizione'];
+            $new_product->type = $product['tipo'];
+            $new_product->image = $product['src'];
+            $new_product->cooking_time = $product['cottura'];
+            $new_product->weight = $product['peso'];
+            $new_product->save();
+
+        }
+    }
+}
